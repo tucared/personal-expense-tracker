80e6a4424ebb8480a0023409ea1d42f616aead1c
Tucared <max.perdrigeat@gmail.com>
2024-12-29 12:18:13 -0300

    build(providers): upgraded providers to latest versions

--- opentofu/provider.tf
+++ opentofu/provider.tf
@@ -4,17 +4,17 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.0.0"
+      version = "6.14.1"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = "3.6.3"
     }
 
     archive = {
       source  = "hashicorp/archive"
-      version = "2.4.0"
+      version = "2.7.0"
     }
   }
 }
