7a562d42646486e17b5b2c6ce8a78f8cf9a082d3
Tucared <max.perdrigeat@gmail.com>
2025-05-03 18:40:50 +0200

    refacto(google_sheets_pipeline): move dlt function calling to main.py

--- opentofu/modules/google_sheets_pipeline/src/main.py
+++ opentofu/modules/google_sheets_pipeline/src/main.py
@@ -1,9 +1,21 @@
+import dlt
 import functions_framework
-
-from google_sheets_pipeline import load_pipeline_with_sheets
+from google_sheets import google_spreadsheet
 
 
 @functions_framework.http
 def google_sheets_pipeline(request):
-    load_pipeline_with_sheets()
+    """
+    Will load all the sheets in the spreadsheet, but it will not load any of the named ranges in the spreadsheet.
+    """
+    pipeline = dlt.pipeline(
+        pipeline_name="google_sheets_pipeline",
+        destination="filesystem",
+        dev_mode=True,
+        dataset_name="sample_google_sheet_data",
+    )
+    data = google_spreadsheet(range_names=["Data"])
+    info = pipeline.run(data)
+    print(info)
+
     return "Pipeline run successfully!"
