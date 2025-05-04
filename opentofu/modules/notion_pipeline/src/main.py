ebf6919b79c5aab857f90560546f56d5240f8261
Tucared <max.perdrigeat@gmail.com>
2025-05-04 12:15:47 +0200

    refactor(notion_pipeline): move dlt function calling to main.py

--- opentofu/modules/notion_pipeline/src/main.py
+++ opentofu/modules/notion_pipeline/src/main.py
@@ -1,9 +1,20 @@
+import dlt
 import functions_framework
-
-from notion_pipeline import load_databases
+from notion import notion_databases
 
 
 @functions_framework.http
 def notion_pipeline(request):
-    load_databases()
+    """Loads all databases from a Notion workspace which have been shared with
+    an integration.
+    """
+    pipeline = dlt.pipeline(
+        pipeline_name="notion",
+        destination="filesystem",
+        dataset_name="notion_data",
+    )
+    data = notion_databases()
+    info = pipeline.run(data)
+    print(info)
+
     return "Pipeline run successfully!"
